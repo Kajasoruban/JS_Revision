d81ec567ca77ae4dab5d502c948a67ce812648b8
Kajasoruban <kajan.bluwin@gmail.com>
2023-11-27 16:27:14 +0530

    first

---
+++ script.js
@@ -0,0 +1,40 @@
+// class Animal {
+//   static belly = [];
+//   eat(e) {
+//     Animal.belly.push(e);
+//   }
+// }
+// let a = new Animal();
+// a.eat("food");
+// a.eat("error");
+// a.eat(1);
+
+
+// console.log(a); 
+
+// var nums=[2,7,11,15],target=9;
+// var output=[];
+// var twoSum = function(nums, target) {
+
+//     for(let i=0;i<nums.length;i++){
+//       var j=i;
+      
+//         for(let k=0;k<nums.length-1;k++){
+//           j++
+//           let m=j%nums.length;
+//           if(nums[i]+nums[m]==target){
+//             if(nums[i]!==nums[m]){
+//               console.log(nums[i],nums[m]);
+//             }
+//           }
+//         }
+        
+        
+//     }
+    
+// };
+
+// twoSum(nums,target);
+
+// console.log(Math.floor(Math.random()*3));
+
