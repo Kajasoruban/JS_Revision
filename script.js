39a61605eaf23300c3cd7328cb43a93d249103be
Kajasoruban <kajan.bluwin@gmail.com>
2023-12-03 22:49:06 +0530

    3rd-dec

--- script.js
+++ script.js
@@ -13,4 +13,25 @@
 // console.log(a); 
 
 
+// const person={
+//     f:"f",
+//     l:"l",
+//     get full(){
+//         return this.f+" "+this.l;
+//     },
+//     set full(v){
+//         const n=v.split(" ");
+//         this.f=n[0];
+//         this.l=n[1];
+//     }
+// }
+// console.log(person.f);
+// person.full="first last";
+// console.log(person.f);
+// console.log(person.full);
+
+
+
+
+// Object.freeze(objectname); will freeze that object
 
